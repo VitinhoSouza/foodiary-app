d487038f8a6b0250a89f09279cdb311535c291f6
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-09-04 17:02:32 -0300

    feat: days navigation, progress calculation and reloads

--- src/app/(private)/meals/[mealId].tsx
+++ src/app/(private)/meals/[mealId].tsx
@@ -26,7 +26,8 @@ export default function MealDetails() {
   const { mealId } = useLocalSearchParams();
 
   const { data: meal, isFetching } = useQuery({
-    queryKey: ["meal"],
+    queryKey: ["meal", mealId],
+    staleTime: Infinity,
     queryFn: async () => {
       const { data } = await httpClient.get<{ meal: Meal }>(`/meals/${mealId}`);
 
